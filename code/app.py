6b830b82dcfb3f8f7e98e70a2f87c102a4f07382
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-13 15:55:29 -0400

    for loop

--- code/app.py
+++ code/app.py
@@ -9,10 +9,8 @@ items = [{'name': 'chair', 'price': 14.50}]
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
         data = request.get_json()
